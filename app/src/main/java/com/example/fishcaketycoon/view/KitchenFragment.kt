05cfa637e95c78110b7273db2d1e11918cf3fbf6
2breadkey <2breadkey@gmail.com>
2021-03-10 02:17:52 +0900

    use androidx fragment

--- app/src/main/java/com/example/fishcaketycoon/view/KitchenFragment.kt
+++ app/src/main/java/com/example/fishcaketycoon/view/KitchenFragment.kt
@@ -2,21 +2,17 @@ package com.example.fishcaketycoon.view
 
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
-import com.example.fishcaketycoon.MainActivity
+import androidx.fragment.app.activityViewModels
 import com.example.fishcaketycoon.R
 import com.example.fishcaketycoon.databinding.FragmentKitchenBinding
-import com.example.fishcaketycoon.model.FishcakeTycoon
 import com.example.fishcaketycoon.viewmodel.FishcakeTycoonViewModel
 
 class KitchenFragment : Fragment(R.layout.fragment_kitchen) {
+    private val viewModel: FishcakeTycoonViewModel by activityViewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentKitchenBinding.bind(view)
-        val viewModel = (requireActivity() as MainActivity).obtainViewModel()
         binding.viewModel = viewModel
     }
 }
